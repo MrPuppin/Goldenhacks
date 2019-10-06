7a52e01651d18b82e60d10b902c6c49ddc1d98a6
Robel-yng <robelnegash2020@gmail.com>
2019-10-05 22:08:03 -0400

    Documentation

--- get_started_complete/popup.js
+++ get_started_complete/popup.js
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 'use strict';
-
+//delete
 let DeleteEverything = document.getElementById('DeleteEverything');
 
 DeleteEverything.onclick = run();
@@ -14,34 +14,35 @@ var i;
 chrome.tts.speak('Caution');
 
 var confirm_action;
-function run(){
-  confirm_action=confirm("You're about to delete your history, cookies, downloads, passwords, localStorage,\ and cache. \nAre you sure you would like to continue?")
-  if(confirm_action==true){
+
+function run() {
+    confirm_action = confirm("You're about to delete your history, cookies, downloads, passwords, localStorage,\ and cache. \nAre you sure you would like to continue?")
+    if (confirm_action == true) {
 
 
-    var millisecondsPer10years = 1000 * 60 * 60 * 24 * 7 * 52 * 10;
-    var tenyearsAgo = (new Date()).getTime() - millisecondsPer10years;
-    chrome.browsingData.remove({
-		'since': tenyearsAgo
-		}, {
-			"appcache": true,
-			"cache": true,
-			"cacheStorage": true,
-			"cookies": true,
-			"downloads": true,
-			"fileSystems": true,
-			"formData": true,
-			"history": true,
-			"indexedDB": true,
-			"localStorage": true,
-			"pluginData": true,
-			"passwords": true,
-			"serviceWorkers": true,
-			"webSQL": true
-		}, callback);
+        var millisecondsPer10years = 1000 * 60 * 60 * 24 * 7 * 52 * 10;
+        var tenyearsAgo = (new Date()).getTime() - millisecondsPer10years;
+        chrome.browsingData.remove({
+            'since': tenyearsAgo
+        }, {
+            "appcache": true,
+            "cache": true,
+            "cacheStorage": true,
+            "cookies": true,
+            "downloads": true,
+            "fileSystems": true,
+            "formData": true,
+            "history": true,
+            "indexedDB": true,
+            "localStorage": true,
+            "pluginData": true,
+            "passwords": true,
+            "serviceWorkers": true,
+            "webSQL": true
+        }, callback);
 
-}else{
-  window.close()
-}
+    } else {
+        window.close()
+    }
 
 };
